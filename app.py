1da8929ad1b27671af49f6ac5638632aca6f3064
rabbithose <157877118+rabbithose@users.noreply.github.com>
2025-05-10 14:16:58 +0900

    Update app.py iris

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st
-import random
-omikuzi = ["大吉","中吉","小吉","凶"]
 
-if st.button("おみくじ"):
-    st.write(random.choice(omikuzi))
+import pandas as pd
+from google.cloud import firestore
+
+db = firestore.Client.from_service_account_json("serviceAccountKey.json")
+
+docs = db.collection("iris").stream()
+data = []
+for d in docs:
+    data.append(d.to_dict())
+spieces = st.sidebar.selectbox("spieces", ["setosa","versicolor","virginica"])
+df = pd.DataFrame(data)
+df = df[df["species"] == spieces].reset_index(drop=True)
+st.write(df)
+st.line_chart(df, x="sepal_length", y="sepal_width")
