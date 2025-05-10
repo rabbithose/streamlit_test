450d3a7832f4ba89490d844a93ca8e843c79f56f
rabbithose <157877118+rabbithose@users.noreply.github.com>
2025-05-10 14:50:39 +0900

    Update app.py test

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import streamlit as st
 
 import pandas as pd
-from google.cloud import firestore
+# from google.cloud import firestore
 
-db = firestore.Client.from_service_account_json("serviceAccountKey.json")
+# db = firestore.Client.from_service_account_json("serviceAccountKey.json")
 
-docs = db.collection("iris").stream()
-data = []
-for d in docs:
-    data.append(d.to_dict())
+# docs = db.collection("iris").stream()
+# data = []
+# for d in docs:
+#     data.append(d.to_dict())
 spieces = st.sidebar.selectbox("spieces", ["setosa","versicolor","virginica"])
-df = pd.DataFrame(data)
-df = df[df["species"] == spieces].reset_index(drop=True)
-st.write(df)
-st.line_chart(df, x="sepal_length", y="sepal_width")
+# df = pd.DataFrame(data)
+# df = df[df["species"] == spieces].reset_index(drop=True)
+# st.write(df)
+# st.line_chart(df, x="sepal_length", y="sepal_width")
